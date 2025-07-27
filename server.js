23b54d0080eb438242bb0268f5d0b99fd7aa6caf
ochi <ocos54ce@gmail.com>
2025-07-27 12:29:11 +0900

    add express

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-import { graphql, buildSchema } from 'graphql';
-
-const schema = buildSchema(`type Query { hello: String } `)
-
-const rootValue = {
+import { buildSchema } from 'graphql';
+import { createHandler } from 'graphql-http/lib/use/express';
+import { ruruHTML } from 'ruru/server';
+import express from 'express';
+ 
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`type Query { hello: String } `);
+ 
+// The root provides a resolver function for each API endpoint
+const root = {
   hello() {
-    return "Hello World"
-  }
-}
+    return 'Hello world!';
+  },
+};
+ 
+const app = express();
 
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue,
-}).then((res) => {
-  console.log(res);
-})
+app.get('/', (_req, res) => {
+  res.type('html');
+  res.end(ruruHTML({ endpoint: '/graphql' }));
+});
+
+// Create and use the GraphQL handler.
+app.all(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  }),
+);
+ 
+// Start the server at port
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
